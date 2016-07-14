615963847ff7958452aa050a7d3ec543c796a079
Rafael Salerno <salerno.rafael@gmail.com>
2016-07-13 23:13:56 -0300

    samples

---
+++ lucid32/manifests/default.pp
@@ -0,0 +1,24 @@
+exec { "update":
+    command => "/usr/bin/apt-get update"
+}
+
+exec { "download-node":
+	require => Exec["update"],
+	command => "/usr/bin/curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -"
+}
+
+exec { "download-docker":
+	require => Package["linux-image-generic-lts-trusty"],
+	command => "/usr/bin/curl -sSL https://get.docker.com/ | sh"
+}
+
+package { "linux-image-generic-lts-trusty":
+	require => Exec["update"],
+	ensure => installed
+}
+
+package { ["nodejs", "git"]:
+    ensure => installed,
+    require => Exec["download-node"]
+}
+Status API Training Shop Blog About Pricing
